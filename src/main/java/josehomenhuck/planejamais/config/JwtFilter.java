6fb01e6f6e2d91a64346b19f4868efcf8385698e
Josepch1 <josepedro.homenhuck@gmail.com>
2024-12-09 20:40:21 -0300

    config: setting up swagger and spring security for work together

---
+++ src/main/java/josehomenhuck/planejamais/config/JwtFilter.java
@@ -0,0 +1,75 @@
+package josehomenhuck.planejamais.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import josehomenhuck.planejamais.application.jwt.JwtService;
+import josehomenhuck.planejamais.domain.user.entity.User;
+import josehomenhuck.planejamais.domain.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+public class JwtFilter extends OncePerRequestFilter {
+  private final JwtService jwtService;
+  private final UserService userService;
+
+    public JwtFilter(JwtService jwtService, UserService userService) {
+        this.jwtService = jwtService;
+        this.userService = userService;
+    }
+
+    @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
+
+    String token = extractToken(request);
+
+    if (token != null) {
+      try {
+        String email = jwtService.getEmailByToken(token);
+        User user = userService.findByEmail(email);
+
+        setUserAsAuthenticated(user);
+      } catch (Exception e) {
+        log.error("Error while authenticating user", e);
+      }
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  private void setUserAsAuthenticated(User user) {
+    UserDetails userDetails = org.springframework.security.core.userdetails.User
+        .withUsername(user.getEmail())
+        .password(user.getPassword())
+        .roles("USER")
+        .build();
+
+    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null,
+        userDetails.getAuthorities());
+
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+  }
+
+  private String extractToken(HttpServletRequest request) {
+    String bearerToken = request.getHeader("Authorization");
+
+    if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+      return bearerToken.split(" ")[1];
+    }
+
+    return null;
+  }
+
+  @Override
+  protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+    return request.getRequestURI().contains("/v1/users");
+  }
+}
---
+++ src/main/java/josehomenhuck/planejamais/domain/user/entity/AccessToken.java
@@ -0,0 +1,12 @@
+package josehomenhuck.planejamais.domain.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccessToken {
+  private String accessToken;
+}
